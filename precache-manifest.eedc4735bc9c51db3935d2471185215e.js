d9e96f687145688b5cf3e2cc2e1181dc600b521c
“Shrenika123” <shrenika.nataraj@gmail.com>
2020-10-10 23:08:16 +0530

    Updates

---
+++ precache-manifest.eedc4735bc9c51db3935d2471185215e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "162948a8fbe4965d2909e2b367eebf39",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "603d879ffa8b109779ae",
+    "url": "/Portfolio/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "76929d3d0a046ad21821",
+    "url": "/Portfolio/static/js/2.363ce9df.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/Portfolio/static/js/2.363ce9df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "603d879ffa8b109779ae",
+    "url": "/Portfolio/static/js/main.15282e6f.chunk.js"
+  },
+  {
+    "revision": "b4f6ea8748b4b447c1e0",
+    "url": "/Portfolio/static/js/runtime-main.d4d59c43.js"
+  }
+]);
